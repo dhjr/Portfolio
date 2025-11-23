1e0e539af4f76fbf1a96a22a066663a472382d7c
dhjr <dhananjayrjs1605@gmail.com>
2025-11-23 19:31:04 +0530

    ui changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-3xl flex flex-col items-center justify-center">
-        <p className=" text-5xl font-josefin font-light">Hey there, I'm</p>
-        <p
-          className={`font-rubik font-bold text-6xl bg-linear-to-b text-transparent from-[#00d19d] to-[#005e48] bg-clip-text`}
-        >
-          Dhananjay
-        </p>
+      <div className="p-2 m-2 w-3xl flex flex-row justify-between">
+        <div className="flex flex-col ">
+          <p className="text-5xl font-josefin ">Hey, I'm</p>
+          <p
+            className={`font-rubik font-bold text-7xl leading-20 bg-linear-to-b text-transparent from-[#00d19d] to-[#005e48] bg-clip-text`}
+          >
+            Dhananjay
+          </p>
+        </div>
         <img
           src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Cat.png"
           alt="Grinning Cat"
-          width="200"
-          height="200"
+          className="size-32"
         />
       </div>
     </>
