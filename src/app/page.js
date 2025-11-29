4ee9ba740597ec104ec7894633f65166e9b235c9
dhjr <dhananjayrjs1605@gmail.com>
2025-11-29 19:13:07 +0530

    added various sections.

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,41 @@
 import Image from "next/image";
+import Projects from "./projects/page";
+import Contact from "./contact/page";
+import Skills from "./skills/page";
 
 export default function Home() {
+  const size = 420;
   return (
     <>
-      <div className="p-2 m-2 w-3xl flex flex-row justify-between">
-        <div className="flex flex-col ">
+      {/* <div className="bg-red-900 w-100 absolute z-0 h-80 top-60 rounded-4xl"></div> */}
+      <div className=" w-full flex flex-row justify-evenly items-center">
+        <Image
+          className=" z-10"
+          width={size}
+          height={size}
+          src={"/profileBgRemoved.png"}
+        ></Image>
+        <div className="flex flex-col justify-center items-start">
           <p className="text-5xl font-josefin ">Hey, I'm</p>
-          <p
-            className={`font-rubik font-bold text-7xl leading-20 bg-linear-to-b text-transparent from-[#00d19d] to-[#005e48] bg-clip-text`}
-          >
-            Dhananjay
-          </p>
+          <div className="flex flex-row items-center">
+            <p
+              className={`font-rubik mr-5 font-bold text-7xl leading-20 bg-linear-to-b text-transparent from-[#00d19d] to-[#005e48] bg-clip-text`}
+            >
+              Dhananjay
+            </p>
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Cat.png"
+              alt="Grinning Cat"
+              className="size-20"
+            />
+          </div>
+          <p>Full Stack Developer</p>
         </div>
-        <img
-          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Grinning%20Cat.png"
-          alt="Grinning Cat"
-          className="size-32"
-        />
       </div>
+
+      <Projects />
+      <Contact />
+      <Skills />
     </>
   );
 }
